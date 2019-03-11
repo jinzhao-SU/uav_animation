7f5ea9a381c6a603078feac5cc8ce223c3ed9000
indecision103 <indecision103@gmail.com>
2019-03-11 15:59:05 -0400

    change url to T

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,11 +11,11 @@ $.ajaxSetup({
     dataType: "json"
 });
 
-$.get("http://ec2-18-191-137-124.us-east-2.compute.amazonaws.com/startarea").done(function (data) {
+$.get("http://ec2-34-226-1-12.compute-1.amazonaws.com/startarea").done(function (data) {
     window.startArea=data;
 });
 
-$.get("http://ec2-18-191-137-124.us-east-2.compute.amazonaws.com/endarea").done(function (data) {
+$.get("http://ec2-34-226-1-12.compute-1.amazonaws.com/endarea").done(function (data) {
     window.endArea=data;
 });
 
@@ -25,7 +25,7 @@ jQuery.ajax({
 console.log("starting async");
 
 
-oboe('http://ec2-18-191-137-124.us-east-2.compute.amazonaws.com/uav')
+oboe('http://ec2-34-226-1-12.compute-1.amazonaws.com/uav')
     .node('{TimeStep ID Latitude Longitude SignalStrength CurrentBasestation finished}', async function (jsonObject) {
         window.uavData.push(jsonObject);
     });
