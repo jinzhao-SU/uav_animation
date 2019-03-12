c337b04891410f31120b950a338d3a6d20e4c451
indecision103 <indecision103@gmail.com>
2019-03-11 22:59:14 -0400

    change url to uavip

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -2720,11 +2720,11 @@ $.ajaxSetup({
     dataType: "json"
 });
 
-$.get("https://utm-nyscorridor.laanc-usa.com/simul/startarea").done(function (data) {
+$.get("https://10.2.1.245:3000/startarea").done(function (data) {
     window.startArea=data;
 });
 
-$.get("https://utm-nyscorridor.laanc-usa.com/simul/endarea").done(function (data) {
+$.get("https://10.2.1.245:3000/endarea").done(function (data) {
     window.endArea=data;
 });
 
@@ -2734,7 +2734,7 @@ jQuery.ajax({
 console.log("starting async");
 
 
-oboe('https://utm-nyscorridor.laanc-usa.com/simul/uav')
+oboe('https://10.2.1.245:3000/uav')
     .node('{TimeStep ID Latitude Longitude SignalStrength CurrentBasestation finished}', async function (jsonObject) {
         window.uavData.push(jsonObject);
     });
