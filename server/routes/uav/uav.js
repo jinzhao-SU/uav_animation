5ba08a16aa9b0d8c4f59bd8345c0c7466e74b898
Wentan Bai <wentan1995@gmail.com>
2019-06-16 15:11:06 -0400

    update new coordinate

--- server/routes/uav/uav.js
+++ server/routes/uav/uav.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-    const readStream=fs.createReadStream("./localdata/uav_coordinate_light.csv");
+    // const readStream=fs.createReadStream("./localdata/uav_coordinate_light.csv");
+    const readStream=fs.createReadStream("./localdata/uav_coordinate_tp.csv");
     await csv().fromStream(readStream).pipe(res);
 });
 
